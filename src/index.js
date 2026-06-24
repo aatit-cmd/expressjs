c39c4f948df4b416757f33cba79cfbac500b31e9
atit <atittulachan6072@gmail.com>
2026-06-24 16:14:41 +0545

    add: first express route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const server = http.createServer(app)
 
 // ip -> 198.168.1.1
 
+//* home -> get, / => <h1>home page</h1>
+// app.get(path, handler)
+app.get('/', (req, res) => {
+    res.send("<h1>Home page</h1>")
+})
+
+//* get all user
+//? get /users ->user page
+app.get('/user', (req, res) => {
+    res.send("<h1>User page</h1>")
+})
+
 server.listen(8080, "localhost", () => {
   console.log(`server is running at http://localhost:8080`);
   console.log("press ctrl+c to close the server");
