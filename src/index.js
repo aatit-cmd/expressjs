e72cf748776d4b524e56a7bb385072447badb984
atit <atittulachan6072@gmail.com>
2026-06-24 16:41:03 +0545

    update: user response to json format

--- src/index.js
+++ src/index.js
@@ -3,59 +3,105 @@ import express from "express";
 const app = express();
 
 //* creating http server
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // ip -> 198.168.1.1
 
 //* home -> get, / => <h1>home page</h1>
 // app.get(path, handler)
-app.get('/', (req, res) => {
-    res.send("<h1>Home page</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1>");
+});
 
 //! CRUD users
 //* get all user
 //? get /user ->user page
-app.get('/users', (req, res) => {
-    res.send("<h1>User page</h1>")
+app.get("/users", (req, res) => {
+//   res.send("<h1>User page</h1>");
+res.json({
+    message: "user fetched",
+    success: true,
+    data : [{
+        _id :1,
+        name : "john doe",
+        email: "john@gmail.com"
+    }]
 })
+});
 
 // create
-app.post('/users',(req,res)=>{
-    res.send("<h1>User created</h1>")
+app.post("/users", (req, res) => {
+//   res.send("<h1>User created</h1>");
+res.json({
+    message: "user created",
+    success: true,
+    data : {
+        _id :1,
+        name : "john doe",
+        email: "john@gmail.com"
+    }
 })
+});
 
 // update
-app.put('/users',(req,res)=>{
-    res.send("<h1>User updated</h1>")
+app.put("/users", (req, res) => {
+//   res.send("<h1>User updated</h1>");
+res.json({
+    message: "user updated",
+    success: true,
+    data : {
+        _id :1,
+        name : "john doe",
+        email: "john@gmail.com"
+    }
 })
+});
 
 // delete
-app.delete('/users',(req,res)=>{
-    res.send("<h1>User deleted</h1>")
+app.delete("/users", (req, res) => {
+//   res.send("<h1>User deleted</h1>");
+res.json({
+    message: "user deleted",
+    success: true,
+    data : {
+        _id :1,
+        name : "john doe",
+        email: "john@gmail.com"
+    }
 })
+});
 
 //! CRUD product
 //* get all product
 //? get /products ->product page
-app.get('/products', (req, res) => {
-    res.send("<h1>Product page</h1>")
-})
+app.get("/products", (req, res) => {
+//   res.send("<h1>Product page</h1>");
+
+});
 
 // create
-app.post('/products',(req,res)=>{
-    res.send("<h1>Product created</h1>")
-})
+app.post("/products", (req, res) => {
+//   res.send("<h1>Product created</h1>");
+});
 
 // update
-app.put('/products',(req,res)=>{
-    res.send("<h1>Product updated</h1>")
+app.put("/products", (req, res) => {
+//   res.send("<h1>Product updated</h1>");
+res.json({
+    message: "user created",
+    success: true,
+    data : {
+        _id :1,
+        name : "john doe",
+        email: "john@gmail.com"
+    }
 })
+});
 
 // delete
-app.delete('/products',(req,res)=>{
-    res.send("<h1>Product deleted</h1>");
-})
+app.delete("/products", (req, res) => {
+  res.send("<h1>Product deleted</h1>");
+});
 
 server.listen(8080, "localhost", () => {
   console.log(`server is running at http://localhost:8080`);
