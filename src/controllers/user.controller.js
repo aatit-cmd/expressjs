6d9b27de27406f897c025b4df616c5d49df4b5f3
atit <atittulachan6072@gmail.com>
2026-07-01 17:01:50 +0545

    add: mongodb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,26 +1,61 @@
+import mongoose from "mongoose";
+
 const users = [];
 
-export const getAll=(req, res) => {
-  //   res.send("<h1>User page</h1>");
-  const query = req.query;
-  console.log(query);
-  // console.log(req.url)
-  // console.log(req.originalUrl)
-  res.status(200).json({
-    message: "user fetched",
-    success: true,
-    // data : [{
-    //     _id :1,
-    //     name : "john doe",
-    //     email: "john@gmail.com"
-    // }]
-    data: users,
-  });
-}
+//! user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-export const getById =  (req, res, next) => {
-  const { id } = req.params; // becasue req.params give object
-  const user = users.find((users) => users._id === Number(id));
+// creating user model
+const User = mongoose.model("users", userSchema);
+
+export const getAll = async (req, res, next) => {
+  try {
+    //   res.send("<h1>User page</h1>");
+    const query = req.query;
+    console.log(query);
+    // console.log(req.url)
+    // console.log(req.originalUrl)
+
+    const users = await User.find({});
+    res.status(200).json({
+      message: "user fetched",
+      success: true,
+      // data : [{
+      //     _id :1,
+      //     name : "john doe",
+      //     email: "john@gmail.com"
+      // }]
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getById = async (req, res, next) => {
+  try{
+    const { id } = req.params; // becasue req.params give object
+  // const user = users.find((users) => users._id === Number(id));
+
+  const user = await User.findOne({_id : id})
 
   if (!user) {
     // res.status(404).json({
@@ -31,7 +66,7 @@ export const getById =  (req, res, next) => {
 
     next({
       message: "user not found",
-      statuscode: 404
+      statuscode: 404,
     });
     return;
   }
@@ -41,30 +76,34 @@ export const getById =  (req, res, next) => {
     success: true,
     data: user,
   });
-}
+  }
+  catch (error){
+    next(error)
+  }
+};
 
 export const create = (req, res, next) => {
   const { name, email, password } = req.body;
 
-  if(!name){
+  if (!name) {
     next({
       message: "name required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
-  if(!email){
+  if (!email) {
     next({
       message: "email required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
-  if(!password){
+  if (!password) {
     next({
       message: "password required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
 
@@ -75,40 +114,40 @@ export const create = (req, res, next) => {
     createdAt: new Date(Date.now()),
     _id: users.length + 1,
   });
-  
+
   res.status(201).json({
     message: "user created",
     success: true,
     data: users[users.length - 1],
   });
-}
+};
 
-export const update= (req, res, next) => {
+export const update = (req, res, next) => {
   //   res.send("<h1>User updated</h1>");
   const { id } = req.params;
   const index = users.findIndex((user) => user._id === Number(id));
-
+  // User.findByIdAndUpdate({_id : id},{name, email, password},{new:true})
   const { name, email, password } = req.body;
 
-  if(!name){
+  if (!name) {
     next({
       message: "name required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
-  if(!email){
+  if (!email) {
     next({
       message: "email required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
-  if(!password){
+  if (!password) {
     next({
       message: "password required",
-      statuscode : 400
-    })
+      statuscode: 400,
+    });
     return;
   }
 
@@ -119,9 +158,9 @@ export const update= (req, res, next) => {
     //   data: null,
     // });
     next({
-      message : "user not found",
-      statuscode : 404
-    })
+      message: "user not found",
+      statuscode: 404,
+    });
     return;
   }
   res.status(200).json({
@@ -134,7 +173,7 @@ export const update= (req, res, next) => {
       password,
     },
   });
-}
+};
 
 export const remove = (req, res) => {
   //   res.send("<h1>User deleted</h1>");
@@ -148,9 +187,9 @@ export const remove = (req, res) => {
     //   data: null,
     // });
     next({
-      message : "user not found",
-      statuscode : 404
-    })
+      message: "user not found",
+      statuscode: 404,
+    });
 
     return;
   }
@@ -160,4 +199,4 @@ export const remove = (req, res) => {
     success: true,
     data: null,
   });
-}
+};
